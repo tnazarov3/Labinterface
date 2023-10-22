5213c8a01dad8c822415684ee24c9f54de62ed14
tnazarov3 <148562555+tnazarov3@users.noreply.github.com>
2023-10-22 17:43:39 +0400

    Update TgBotDosSmr.py

--- TgBotDosSmr.py
+++ TgBotDosSmr.py
@@ -1,22 +1,19 @@
-"""
 import telebot
 from telebot import types
 from telebot.types import KeyboardButton, ReplyKeyboardMarkup, WebAppInfo
 
-#bot = telebot.TeleBot('6362714649:AAEOjpU0NL6pu_DypST1JLIybi9U_WvxVMM')
+bot = telebot.TeleBot('6362714649:AAEOjpU0NL6pu_DypST1JLIybi9U_WvxVMM')
 
-web_app = WebAppInfo(url="https://tnazarov3.github.io/LabInterface")
-@bot.message_handler(commands=['start'])
-async def start(message: types.Message):
-    markup = types.ReplyKeyboardMarkup()
-    markup.add(types.KeyboardButton('Открыть', web_app = web_app)
-    await message.answer('l', reply_markup=markup)
-#keyboard = ReplyKeyboardMarkup(
-    #keyboard=[
-        #[KeyboardButton(text="Достопримечательности Самары", web_app=web_app)]
-    #],
-    #resize_keyboard=True
-#)
+web_app = WebAppInfo(url="https://tnazarov3.github.io/Labinterface/")
+
+
+@bot.message_handler(content_types=['text'])
+def start(message: types.Message):
+    if message.text == '/start':
+        markup = types.ReplyKeyboardMarkup()
+        markup.add(types.KeyboardButton('Открыть', web_app=web_app))
+        bot.send_message(message.from_user.id, '1', reply_markup=markup)
+
 
 DISC = {
     "1": "Благоприятными для Овнов в этот день будут физические",
@@ -26,8 +23,7 @@ DISC = {
 
 @bot.message_handler(content_types='web_app_data')
 def buy_process(web_app_message):
-    bot.send_message(web_app_message.chat.id, DISC[f'{web_app_message.web_app_data}'])
+    bot.send_message(web_app_message.chat.id, DISC[f'{web_app_message.web_app_data.data}'])
 
-#bot.polling(none_stop=True, interval=0)
-    
-"""
+
+bot.polling(none_stop=True, interval=0)
